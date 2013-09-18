93796168662f6b2056e952d969eec01d5471e57f
Karissa McKelvey <krmckelv@gmail.com>
2013-09-18 00:10:22 -0500

    setup heroku stuff

--- server.py
+++ server.py
@@ -1,8 +1,4 @@
 #!/usr/bin/env python
 from papertalk import papertalk
 
-def app():
-    papertalk.run(port=4324, debug = True)
-
-if __name__ == "__main__":
-    app()
+papertalk.run(port=4324, debug = True)
