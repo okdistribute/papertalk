095b303ed897762f5b20cf4978377bcaf1a71686
Karissa McKelvey <krmckelv@gmail.com>
2013-09-18 18:50:29 -0500

    add article

--- papertalk/views.py
+++ papertalk/views.py
@@ -4,3 +4,10 @@ from papertalk import papertalk
 @papertalk.route('/')
 def index():
 	return render_template('index.html')
+
+@papertalk.route('/article/<int:id>')
+def article(id):
+    context = {}
+    context["article"] = {'id': id, 'title': "hello" }
+    return render_template('article.html', **context)
+
