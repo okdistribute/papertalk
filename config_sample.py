e8a9c479810cff508b41dce695a7181b454a0cd0
Karissa McKelvey <krmckelv@gmail.com>
2013-10-06 12:10:14 -0500

    using sample config

---
+++ config_sample.py
@@ -0,0 +1,11 @@
+class Config(object):
+    """
+    Config for papertalk. Rename to config.py before running server
+    """
+    DEBUG = True
+    TESTING = True
+    MONGO_HOST = ''
+    MONGO_PORT = 0000
+    MONGO_DBNAME = ''
+    MONGO_USERNAME = ''
+    MONGO_PASSWORD = ''
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 from papertalk import papertalk
 
-papertalk.run(port=4324, debug = True)
+papertalk.run(port=4324, debug=True)
 
