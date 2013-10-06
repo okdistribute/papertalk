9915457d36d1edb37b2e2239b746a6a063032302
Karissa McKelvey <krmckelv@gmail.com>
2013-10-06 12:18:10 -0500

    fix circular dependency

--- papertalk/models/article.py
+++ papertalk/models/article.py
@@ -1,8 +1,5 @@
-from papertalk.utils import utils, scholar
-from datetime import datetime
 from papertalk import mongo
 from papertalk.models.sites import Scholar
-import re
 
 
 class Article(object):
@@ -52,25 +49,23 @@ class Article(object):
 
         return article_id
 
-    @classmethod
-    def search(cls, text):
-        """
-        Parses the article from a given search string.
-        This could be a title (most likely); or an author
-        """
-        siteClass = Scholar
+def search(cls, text):
+    """
+    Parses the article from a given search string.
+    This could be a title (most likely); or an author
+    """
+    siteClass = Scholar
 
-        return siteClass.search(text=text)
+    return siteClass.search(text=text)
 
-    @classmethod
-    def from_url(cls, site, url):
-        """
-        Scrapes the url and returns a new article
-        """
-        ##TODO right now, it always assumes google scholar. need to check
-        ## which site it is and load appropriately
-        siteClass = Scholar
+def from_url(cls, site, url):
+    """
+    Scrapes the url and returns a new article
+    """
+    ##TODO right now, it always assumes google scholar. need to check
+    ## which site it is and load appropriately
+    siteClass = Scholar
 
 
-        return siteClass.scrape(url)
+    return siteClass.scrape(url)
 
