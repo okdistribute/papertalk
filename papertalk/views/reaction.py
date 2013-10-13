8e96ff6184149a6b319bc6e0455d9597ca3b016c
Beau Cronin <beau.cronin@gmail.com>
2013-10-13 14:51:35 -0700

    basic reaction saving to mongo

--- papertalk/views/reaction.py
+++ papertalk/views/reaction.py
@@ -1,6 +1,7 @@
 from flask import render_template, request
 from papertalk import papertalk, mongo
 from papertalk.utils.utils import jsonify
+from papertalk.models.reaction import Reaction
 
 @papertalk.route('/reaction/<int:id>')
 def reaction(id):
@@ -8,8 +9,17 @@ def reaction(id):
     context["reaction"] = mongo.db.find_one({"_id" : id})
     return render_template('reaction.html', **context)
 
-@papertalk.route('/reaction/new')
+@papertalk.route('/reaction/new', methods=["GET", "POST"])
 def reaction_author():
-    context = {}
-    return render_template('reaction_author.html', **context)
+    if request.method == "GET":
+        context = {}
+        return render_template('reaction_author.html', **context)
+    elif request.method == "POST":
+        reaction = Reaction()
+        reaction['title'] = request.form['title'].strip()
+        reaction['body'] = request.form['text']
+        reaction.save()
+        return jsonify({'status': 'reaction saved'})
+
+
 
