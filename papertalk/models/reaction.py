9e5b8838c99ec5bba42dfa8eccad0aac7ff180ef
Karissa McKelvey <krmckelv@gmail.com>
2013-10-15 22:33:36 -0500

    FIX: cleaned up the reaction model so that save also acts as update

--- papertalk/models/reaction.py
+++ papertalk/models/reaction.py
@@ -9,11 +9,10 @@ class Reaction(object):
     """
 
     def __init__(self):
-        self.attrs = {'id':            None,
+        self.attrs = {'_id':            None,
                       'title':         None,
                       'body':          None,
                       'user':          None}
-
 
     def __getitem__(self, key):
         return self.attrs[key]
@@ -24,23 +23,28 @@ class Reaction(object):
     def __delitem__(self, key):
         del self.attrs[key]
 
+    def _create(self):
+        self.attrs["_id"] = mongo.db.reactions.insert(self.attrs)
+        return self.attrs["_id"]
+
     def save(self):
-        if self['id']:
-            reaction = mongo.db.reactions.find_one({"_id" : self["id"]})
+        """
+        Called to save or update the reaction.
+        """
+        if self.attrs['_id']:
+            reaction = mongo.db.reactions.find_one({"_id" : self["_id"]})
 
             if reaction:
-                print "reaction exists"
-                reaction_id = reaction["_id"]
+                print "updating reaction"
+                print self.as_txt()
+                mongo.db.reactions.save(self.attrs)
             else:
-                print "reaction id not found"
-                # FIXME
-                reaction_id = None
-
+                raise Exception("weird. this reaction must have been deleted before an update.")
         else:
             print "creating new reaction"
-            reaction_id = mongo.db.reactions.insert(self.attrs)
+            self._create()
 
-        return reaction_id
+        return self.attrs["_id"]
 
     def as_txt(self):
         # Get items sorted in specified order:
--- papertalk/views/reaction.py
+++ papertalk/views/reaction.py
@@ -14,6 +14,7 @@ def reaction_author():
     if request.method == "GET":
         context = {}
         return render_template('reaction_author.html', **context)
+
     elif request.method == "POST":
         reaction = Reaction()
         reaction['title'] = request.form['title'].strip()
