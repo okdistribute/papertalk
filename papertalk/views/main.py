255812bbaf56e524e7125dbee05f81fbd7b63865
Beau Cronin <beau.cronin@gmail.com>
2013-10-20 18:52:26 -0700

    routes for about and get-involved

--- papertalk/views/main.py
+++ papertalk/views/main.py
@@ -5,5 +5,12 @@ from papertalk import papertalk
 def index():
 	return render_template('index.html')
 
+@papertalk.route('/about')
+def about():
+	return render_template('about.html')
+
+@papertalk.route('/get-involved')
+def get_involved():
+	return render_template('get-involved.html')
 
 
