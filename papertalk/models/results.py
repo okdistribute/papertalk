aee5b1389566e5ca1c5003690d897d1ae58877b6
Karissa McKelvey <krmckelv@gmail.com>
2013-10-29 01:30:02 -0500

    yes I agree, remove this

--- papertalk/models/results.py
+++ papertalk/models/results.py
@@ -1,20 +1,6 @@
 __author__ = 'karissamckelvey'
 from papertalk.models.article import Article
 
-# TODO deprecated; remove?
-def get_known_articles(articles):
-    """
-    Return the known articles from an article result query
-    """
-    res = []
-    for article in articles:
-        try:
-            a = Article.lookup({"title" : article["title"], "year": article["year"]})
-        except:
-            a = article
-
-        res.append(a)
-    return res
 
 def get_or_insert_articles(search_results):
     res = []
