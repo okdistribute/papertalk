7dbf49f8b47f1e0f065f570175ed2281f1ae54fd
Karissa McKelvey <krmckelv@gmail.com>
2013-11-05 11:20:31 -0600

    fix the routes to work with changes reflected in master

--- papertalk/views/main.py
+++ papertalk/views/main.py
@@ -6,11 +6,11 @@ main_blueprint  = Blueprint("main", __name__)
 def index():
 	return render_template('index.html')
 
-@papertalk.route('/about')
+@main_blueprint.route('/about')
 def about():
 	return render_template('about.html')
 
-@papertalk.route('/get-involved')
+@main_blueprint.route('/get-involved')
 def get_involved():
 	return render_template('get-involved.html')
 
