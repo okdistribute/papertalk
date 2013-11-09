c8935f8464ea6541ab91387793d13ef6fa249d7a
Karissa McKelvey <krmckelv@gmail.com>
2013-11-09 15:13:53 -0500

    add WTForms for validation of reaction enter

--- papertalk/views/reaction.py
+++ papertalk/views/reaction.py
@@ -1,33 +1,30 @@
 from flask import render_template, request, Blueprint, redirect
 from papertalk.models import reactions, articles
+from papertalk.forms import ReactionForm
 
 reaction_blueprint  = Blueprint("reaction", __name__)
 
 @reaction_blueprint.route('/reaction/<id>', methods=["GET"])
 def reaction(id):
     context = {}
-    r = reactions.lookup(_id=id)
-    context["reaction"] = r
-    context["article"] = articles.lookup(_id=r['article_id'])
+    reaction = reactions.lookup(_id=id)
+    context["reaction"] = reaction
+    context["article"] = articles.lookup(_id=reaction['article_id'])
     return render_template('reaction.html', **context)
 
 
-@reaction_blueprint.route('/reaction/new', methods=["GET", "POST"])
-def reaction_author():
-    if request.method == "GET":
-        context = {}
-        context['article_id'] = request.args.get('article')
-        return render_template('reaction_author.html', **context)
+@reaction_blueprint.route('/article/<article_id>/reaction/new', methods=["GET", "POST"])
+def reaction_author(article_id):
+    form = ReactionForm(request.form)
 
-    elif request.method == "POST":
-        print "HELLO"
-        title = request.form['title'].strip()
-        body = request.form['body']
-        article_id = request.form['article_id']
-        reactions.save(title=title,
-                       body=body,
+    if request.method == "POST" and form.validate:
+        reactions.save(title=form.title.data,
+                       body=form.body.data,
                        article_id=article_id)
-
         return redirect("/article/%s" % article_id)
 
+    context = {}
+    context['article_id'] = article_id
+    context['form'] = form
+    return render_template('reaction_author.html', **context)
 
