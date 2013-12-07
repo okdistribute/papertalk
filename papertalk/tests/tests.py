c64710862a80472f4c2494c0a4d51f1f4d7ea01b
Karissa McKelvey <krmckelv@gmail.com>
2013-12-06 18:02:44 -0600

    fixing up stests..

--- papertalk/tests/tests.py
+++ papertalk/tests/tests.py
@@ -6,19 +6,6 @@ from papertalk.tests import PapertalkTestCase
 
 class TestArticle(PapertalkTestCase):
 
-    def testFromScholarURL(self):
-        """
-        Should get an article from google scholar
-        """
-        url = "http://scholar.google.com/citations?view_op=view_citation&hl=en&user=RM2tB8EAAAAJ&citation_for_view=RM2tB8EAAAAJ:u5HHmVD_uO8C"
-        article = Scholar.scrape(url)
-
-        self.assertEqual(article["title"], "Visualizing Communication on Social Media: Making Big Data Accessible")
-        self.assertEqual(article["direct_url"], "http://arxiv.org/abs/1202.1367")
-        self.assertEqual(article["authors"], ["Karissa McKelvey", "Alex Rudnick", "Michael D Conover", "Filippo Menczer"])
-        self.assertEqual(article["num_citations"], 7)
-        self.assertEqual(article["year"], 2012)
-
     def testMendeley(self):
         text = "visualizing communication on social media: making big data accessible"
         articles = Mendeley.search(text)
@@ -43,8 +30,6 @@ class TestArticle(PapertalkTestCase):
         View the article
         """
         article = self.get_article()
-        print article
-
         res = self.client.get("/article/view/%s" % article["_id"])
         self.assert200(res)
 
